ef4eb9f1b29189394fe4a1811d9eb5671d85e4f3
Ganesh Tiwari <ganesht049@gmail.com>
2023-08-10 22:20:38 +0530

    feat: added required and optional variable

--- script.sh
+++ script.sh
@@ -1,6 +1,16 @@
 echo "Hello From Script"
-for i in $LIST
-do
-    echo "Welcome $i times"
+yq '.secrets.read_access[]' secrets-config.yaml | while read -r secret_name; do
+  role_to_attach="roles/secretmanager.secretAccessor"
+  echo "Binding $role_to_attach for $secret_name"
+  gcloud secrets add-iam-policy-binding projects/bhdigital-data-dev/secrets/$secret_name \
+  --member "serviceAccount:test-iam-conditions@bhdigital-data-dev.iam.gserviceaccount.com" \
+  --role $role_to_attach
 done
 
+yq '.secrets.write_access[]' secrets-config.yaml | while read -r secret_name; do
+  role_to_attach="roles/secretmanager.secretVersionManager"
+  echo "Binding $role_to_attach for $secret_name" 
+  gcloud secrets add-iam-policy-binding projects/bhdigital-data-dev/secrets/$secret_name \
+  --member "serviceAccount:test-iam-conditions@bhdigital-data-dev.iam.gserviceaccount.com" \
+  --role $role_to_attach
+done
